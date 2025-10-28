328f1b3d9b330e4c77909b8124c8d0557f7ae055
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-10-29 00:23:14 +0700

    P9_Percobaan 1

---
+++ SiakadFor16.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class SiakadFor16 {
+
+    public static void main(String[] args) {
+        Scanner dito = new Scanner(System.in);
+        double nilai16, tertinggi16= 0, terendah16= 100, lulus16=0, tidakLulus16=0;
+        int i;
+        for (i=1; i <= 10; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-"+ i + ": ");
+            nilai16 = dito.nextDouble();
+            if (nilai16 > tertinggi16) {
+                tertinggi16 = nilai16;
+
+            }
+            // if (nilai16 < terendah16) {
+            //     terendah16 = nilai16;
+            // }
+            // if (nilai16 >= 60){
+            //     lulus16++;
+            // }
+            // if (nilai16 < 60){
+            //     tidakLulus16++;
+            // }
+        }
+        System.out.println("Nilai Tertinggi : "+ tertinggi16);
+        System.out.println("Nilai Terendah : " + terendah16);
+        System.out.println("Lulus: "+ lulus16);
+        System.out.println("Tidak Lulus"+ tidakLulus16);
+        
+    }
+}
